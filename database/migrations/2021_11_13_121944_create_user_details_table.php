dc87410bd52da7c000b545cbb6081aa80e1e5c50
Martin Crnjakovic <martin.crnjakovic@thesiestagroup.com>
2021-11-13 13:45:18 +0100

    #000 [feat] UserDetails Migration

---
+++ database/migrations/2021_11_13_121944_create_user_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('citizenship_country_id')->constrained('countries');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
