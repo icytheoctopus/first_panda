b2003b1f89fcf3273f96df306ecd8bb48354d7d4
Martin Crnjakovic <martin.crnjakovic@thesiestagroup.com>
2021-11-14 11:14:38 +0100

    #000 [feat] Removed relationship from Country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,16 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
-
-    public function user_details(): HasMany
-    {
-        return $this->hasMany(UserDetails::class, 'citizenship_country_id');
-    }
 }
